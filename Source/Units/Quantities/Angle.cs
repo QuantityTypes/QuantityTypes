e25cfa942f0f7f3a400ca5f7afd538a4a0feda74
Oystein Bjorke <oystein.bjorke@gmail.com>
2012-10-26 10:58:43 +0200

    Corrected Angle.Degree multiplier

--- Source/Units/Quantities/Angle.cs
+++ Source/Units/Quantities/Angle.cs
@@ -1,6 +1,27 @@
 // --------------------------------------------------------------------------------------------------------------------
 // <copyright file="Angle.cs" company="Units.NET">
+//   The MIT License (MIT)
+//   
 //   Copyright (c) 2012 Oystein Bjorke
+//   
+//   Permission is hereby granted, free of charge, to any person obtaining a
+//   copy of this software and associated documentation files (the
+//   "Software"), to deal in the Software without restriction, including
+//   without limitation the rights to use, copy, modify, merge, publish,
+//   distribute, sublicense, and/or sell copies of the Software, and to
+//   permit persons to whom the Software is furnished to do so, subject to
+//   the following conditions:
+//   
+//   The above copyright notice and this permission notice shall be included
+//   in all copies or substantial portions of the Software.
+//   
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // </copyright>
 // <summary>
 //   Represents the angle quantity.
@@ -30,7 +51,7 @@ namespace Units
         /// <summary>
         /// The backing field for the <see cref="Degree" /> property.
         /// </summary>
-        private static readonly Angle DegreeField = new Angle(180 / Math.PI);
+        private static readonly Angle DegreeField = new Angle(Math.PI / 180);
 
         /// <summary>
         /// The value.
