54b33d770abc39dfbd1292ccbdf824a5e960e2c3
objo <objo@localhost>
2012-11-23 15:03:40 +0100

    Added TypographicLength and TypographicResolution

---
+++ Source/Units/Operators/TypographicResolution.cs
@@ -0,0 +1,28 @@
+﻿namespace Units
+{
+    public partial struct TypographicResolution
+    {
+        /// <summary>
+        ///     Implements the operator /.
+        /// </summary>
+        /// <param name="x"> The x. </param>
+        /// <param name="y"> The y. </param>
+        /// <returns> The result of the operator. </returns>
+        public static TypographicLength operator /(double x, TypographicResolution y)
+        {
+            return new TypographicLength(x / y.Value);
+        }
+
+        /// <summary>
+        ///     Implements the operator *.
+        /// </summary>
+        /// <param name="x"> The x. </param>
+        /// <param name="y"> The y. </param>
+        /// <returns> The result of the operator. </returns>
+        public static double operator *(TypographicResolution x, TypographicLength y)
+        {
+            return x.Value * y.Value;
+        }
+
+    }
+}
