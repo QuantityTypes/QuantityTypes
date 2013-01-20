43f4988d006ed86128ed627ae681247359e468a9
Oystein Bjorke <oystein.bjorke@gmail.com>
2013-01-20 21:58:52 +0100

    Added CsvIgnore attribute

---
+++ Source/Units/Utilities/CsvIgnoreAttribute.cs
@@ -0,0 +1,59 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CsvColumnAttribute.cs" company="Units.NET">
+//   The MIT License (MIT)
+//   
+//   Copyright (c) 2012 Oystein Bjorke
+//   
+//   Permission is hereby granted, free of charge, to any person obtaining a
+//   copy of this software and associated documentation files (the
+//   "Software"), to deal in the Software without restriction, including
+//   without limitation the rights to use, copy, modify, merge, publish,
+//   distribute, sublicense, and/or sell copies of the Software, and to
+//   permit persons to whom the Software is furnished to do so, subject to
+//   the following conditions:
+//   
+//   The above copyright notice and this permission notice shall be included
+//   in all copies or substantial portions of the Software.
+//   
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+// <summary>
+//   Specifies the column index in a csv file of the decorated property.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Units
+{
+    using System;
+    using System.ComponentModel;
+
+    /// <summary>
+    /// Specifies that a property should be ignored in csv output.
+    /// </summary>
+    public class CsvIgnoreAttribute : Attribute
+    {
+        /// <summary>
+        /// Determines whether the specified property has the CsvIgnored attribute.
+        /// </summary>
+        /// <param name="propertyDescriptor">The property descriptor.</param>
+        public static bool IsIgnored(PropertyDescriptor propertyDescriptor)
+        {
+            return propertyDescriptor.Attributes[typeof(CsvIgnoreAttribute)] != null;
+        }
+
+        /// <summary>
+        /// Determines whether the specified property does not have the CsvIgnored attribute.
+        /// </summary>
+        /// <param name="propertyDescriptor">The property descriptor.</param>
+        public static bool IsNotIgnored(PropertyDescriptor propertyDescriptor)
+        {
+            return !IsIgnored(propertyDescriptor);
+        }
+    }
+}
