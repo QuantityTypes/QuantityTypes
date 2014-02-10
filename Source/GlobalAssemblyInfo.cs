df4604fbc733ee8fbb0860f40705bae681d67a6f
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-02-10 21:27:07 +0100

    Update version number to 2014.1

--- Source/GlobalAssemblyInfo.cs
+++ Source/GlobalAssemblyInfo.cs
@@ -9,9 +9,9 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Units for .NET")]
 
-[assembly: AssemblyCopyright("Copyright © Units for .NET 2013")]
+[assembly: AssemblyCopyright("Copyright © Units for .NET 2012-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2013.1.1.*")]
-[assembly: AssemblyFileVersion("2013.1.1.0")]
+[assembly: AssemblyVersion("2014.1.1.*")]
+[assembly: AssemblyFileVersion("2014.1.1.0")]
