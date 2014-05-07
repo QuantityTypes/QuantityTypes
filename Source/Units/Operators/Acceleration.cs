1c7269f2a5c889ff635eaaa23a29534de13e2335
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-05-07 14:51:10 +0200

    Add TimeSquared and VelocitySquared

--- Source/Units/Operators/Acceleration.cs
+++ Source/Units/Operators/Acceleration.cs
@@ -42,7 +42,29 @@ namespace Units
         /// <returns> The result of the operator. </returns>
         public static Velocity operator *(Acceleration x, Time y)
         {
-            return new Velocity(x.Value / y.Value);
+            return new Velocity(x.Value * y.Value);
+        }
+
+        /// <summary>
+        /// Implements the * operator for the product of <see cref="Acceleration" /> and <see cref="Length" />.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <returns>The result of the operator.</returns>
+        public static VelocitySquared operator *(Acceleration x, Length y)
+        {
+            return new VelocitySquared(x.Value * y.Value);
+        }
+
+        /// <summary>
+        /// Implements the * operator for the product of <see cref="Acceleration" /> and <see cref="TimeSquared" />.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <returns>The result of the operator.</returns>
+        public static Length operator *(Acceleration x, TimeSquared y)
+        {
+            return new Length(x.Value * y.Value);
         }
     }
 }
