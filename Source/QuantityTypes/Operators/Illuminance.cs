a30f45bffb71f1763cb45bb66845accbd41f5878
Nils Haferkemper <nils@haferkemper.de>
2015-11-23 22:32:41 +0100

    added lighting calculations

---
+++ Source/QuantityTypes/Operators/Illuminance.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Illuminance.cs" company="QuantityTypes">
+//   Copyright (c) 2014 QuantityTypes contributors
+// </copyright>
+// <summary>
+//   Provides operators related to illuminance.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace QuantityTypes
+{
+    using System;
+
+    /// <summary>
+    ///     Provides operators related to illuminance.
+    /// </summary>
+    public partial struct Illuminance
+    {
+        /// <summary>
+        ///     Implements the operator *.
+        ///     I=E*A
+        /// </summary>
+        /// <param name="l1"> The illuminance. </param>
+        /// <param name="l2"> The area. </param>
+        /// <returns> The luminous flux through an area. </returns>
+        public static LuminousFlux operator *(Illuminance l1, Area l2)
+        {
+            return new LuminousFlux(l1.value * l2.Value);
+        }
+
+        /// <summary>
+        ///     Implements the operator *.
+        ///     I=E*r²
+        /// </summary>
+        /// <param name="l1"> The illuminance. </param>
+        /// <param name="l2"> The distance. </param>
+        /// <returns> Luminous intensity generating the illuminance in that distance </returns>
+        public static LuminousIntensity operator *(Illuminance l1, Length l2)
+        {
+            return new LuminousIntensity(l1.value * l2.Squared().Value);
+        }
+    }
+}
