054572f264a72e748558f8ec55e3365340b97160
joseph-bs <jschwarzbach@brightsourceenergy.com>
2016-03-08 15:59:58 +0200

    Improvements to Temperature type + adding an operator
    Merged the changes made to other types into Temperature type while
    keeping the particularities of this type. Added an operator. Updated
    tests.

--- Source/QuantityTypes/Operators/Temperature.cs
+++ Source/QuantityTypes/Operators/Temperature.cs
@@ -31,6 +31,17 @@ namespace QuantityTypes
         /// <param name="x"> The x. </param>
         /// <param name="y"> The y. </param>
         /// <returns> The result of the operator. </returns>
+        public static Temperature operator -(Temperature x, TemperatureDifference y)
+        {
+            return new Temperature(x.value - y.Value);
+        }
+        
+        /// <summary>
+        ///     Implements the operator -.
+        /// </summary>
+        /// <param name="x"> The x. </param>
+        /// <param name="y"> The y. </param>
+        /// <returns> The result of the operator. </returns>
         public static TemperatureDifference operator -(Temperature x, Temperature y)
         {
             return new TemperatureDifference(x.value - y.value);
