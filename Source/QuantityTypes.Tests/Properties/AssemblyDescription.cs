2fbad46e26b546055697bb4f5b74a882bf8b5da3
Oystein Bjorke <oystein.bjorke@gmail.com>
2016-09-19 20:56:29 +0200

    Add GitVersion to build script

---
+++ Source/QuantityTypes.Tests/Properties/AssemblyDescription.cs
@@ -0,0 +1,11 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="QuantityTypes">
+//   Copyright (c) 2014 QuantityTypes contributors
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+
+[assembly: AssemblyProduct("QuantityTypes")]
+[assembly: AssemblyTitle("QuantityTypes unit test library")]
+[assembly: AssemblyDescription("Unit tests.")]
