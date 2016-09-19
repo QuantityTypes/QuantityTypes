21c8acd4abb0f1bc8679df9c8271b4edef22efd5
Oystein Bjorke <oystein.bjorke@gmail.com>
2016-09-19 20:38:10 +0200

    Fix build warnings

--- Source/QuantityTypes/Operators/Fraction.cs
+++ Source/QuantityTypes/Operators/Fraction.cs
@@ -3,7 +3,7 @@
 //   Copyright (c) 2014 QuantityTypes contributors
 // </copyright>
 // <summary>
-//   Implements operators related to volume.
+//   Implements operators related to Fraction.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -14,27 +14,27 @@ namespace QuantityTypes
     /// </summary>
     public partial struct Fraction
     {
-       /// <summary>
-       ///     Performs an implicit conversion from <see cref="Fraction" /> to <see cref="MassConcentrationInWater" />.
-       /// </summary>
-       /// <param name="c"> The fraction. </param>
-       /// <returns> MassConcentration. </returns>
-       public static implicit operator MassConcentrationInWater(Fraction f)
+        /// <summary>
+        ///     Performs an implicit conversion from <see cref="Fraction" /> to <see cref="MassConcentrationInWater" />.
+        /// </summary>
+        /// <param name="f"> The fraction. </param>
+        /// <returns> MassConcentration. </returns>
+        public static implicit operator MassConcentrationInWater(Fraction f)
         {
-           // Base unit of MassConcentrationInWater is kg/m^3. 1mg/L == 1 ppm.
-           return MassConcentrationInWater.MilligramPerLitre * f.ConvertTo(Fraction.PartPerMillion);
+            // Base unit of MassConcentrationInWater is kg/m^3. 1mg/L == 1 ppm.
+            return MassConcentrationInWater.MilligramPerLitre * f.ConvertTo(Fraction.PartPerMillion);
         }
 
-       /// <summary>
-       ///     Performs an implicit conversion from <see cref="Fraction" /> to <see cref="VolumetricWaterContent" />.
-       /// </summary>
-       /// <param name="c"> The fraction. </param>
-       /// <returns> VolumetricWaterContent. </returns>
-       public static implicit operator VolumetricWaterContent(Fraction f)
-       {
-          // Base unit of VolumetricWaterContent m^3/m^3.
-          return new VolumetricWaterContent(f.value);
-       }
+        /// <summary>
+        ///     Performs an implicit conversion from <see cref="Fraction" /> to <see cref="VolumetricWaterContent" />.
+        /// </summary>
+        /// <param name="f"> The fraction. </param>
+        /// <returns> VolumetricWaterContent. </returns>
+        public static implicit operator VolumetricWaterContent(Fraction f)
+        {
+            // Base unit of VolumetricWaterContent m^3/m^3.
+            return new VolumetricWaterContent(f.value);
+        }
 
     }
 }
