19865e98b42358df3022d97a31602c8869226c70
Oystein Bjorke <oystein.bjorke@dnvgl.com>
2016-10-12 15:46:45 +0200

    add version number to project.json

--- Source/QuantityTypes/Properties/AssemblyInfo.cs
+++ Source/QuantityTypes/Properties/AssemblyInfo.cs
@@ -9,5 +9,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.1.0")]
+[assembly: AssemblyFileVersion("0.9.2.0")]
