dcd7de29fbe341c3eb9bd2d9761ee1a6eb7912f3
Oystein Bjorke <oystein.bjorke@gmail.com>
2016-10-16 14:00:13 +0200

    Add imperial volume units

---
+++ Source/QuantityTypes.Tests/Quantities/VolumeTests.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="VolumeTests.cs" company="QuantityTypes">
+//   Copyright (c) 2014 QuantityTypes contributors
+// </copyright>
+// <summary>
+//   Provides unit test for the Volume class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Globalization;
+
+namespace QuantityTypes.Tests
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Provides unit test for the <see cref="Volume" /> class.
+    /// </summary>
+    public class VolumeTests
+    {
+        [Test]
+        public void ImperialUnits()
+        {
+            var imperialUnitProvider = new UnitProvider(CultureInfo.InvariantCulture);
+            imperialUnitProvider.RegisterUnits(typeof(Imperial));
+
+            Assert.AreEqual(Imperial.Gallon, Volume.Parse("1 gal", imperialUnitProvider));
+            Assert.AreEqual(Imperial.FluidOunce, Volume.Parse("1 floz", imperialUnitProvider));
+
+            Assert.AreNotEqual(Imperial.Gallon, Volume.Gallon);
+        }
+    }
+}
---
+++ Source/QuantityTypes/Units/Imperial.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Imperial.cs" company="QuantityTypes">
+//   Copyright (c) 2014 QuantityTypes contributors
+// </copyright>
+// <summary>
+//   Provides imperial (UK) units.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace QuantityTypes
+{
+    /// <summary>
+    /// Provides imperial (UK) units.
+    /// </summary>
+    /// <remarks>See <a href="https://en.wikipedia.org/wiki/Imperial_units"> Imperial units</a>.</remarks>
+    public class Imperial
+    {
+        /// <summary>
+        /// Gets the imperical fluid ounce unit.
+        /// </summary>
+        [Unit("floz")]
+        public static Volume FluidOunce { get; } = new Volume(28.4130625e-6);
+
+        /// <summary>
+        /// Gets the imperical gill unit.
+        /// </summary>
+        [Unit("gi")]
+        public static Volume Gill { get; } = 5 * FluidOunce;
+
+        /// <summary>
+        /// Gets the imperical pint unit.
+        /// </summary>
+        [Unit("pt")]
+        public static Volume Pint { get; } = 20 * FluidOunce;
+
+        /// <summary>
+        /// Gets the imperical quart unit.
+        /// </summary>
+        [Unit("qt")]
+        public static Volume Quart { get; } = 40 * FluidOunce;
+
+        /// <summary>
+        /// Gets the imperical gallon unit.
+        /// </summary>
+        [Unit("gal")]
+        public static Volume Gallon { get; } = 160 * FluidOunce;
+    }
+}
