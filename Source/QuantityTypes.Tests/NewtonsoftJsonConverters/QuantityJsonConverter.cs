d877a7b3f8c49f78b88a86108508ac740c746ddd
Oystein Bjorke <oystein.bjorke@gmail.com>
2022-05-23 10:42:08 +0200

    add tests using System.Text.Json

---
+++ Source/QuantityTypes.Tests/NewtonsoftJsonConverters/QuantityJsonConverter.cs
@@ -0,0 +1,61 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="QuantityJsonConverter.cs" company="QuantityTypes">
+//   Copyright (c) 2014 QuantityTypes contributors
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace QuantityTypes.Tests
+{
+    using System;
+    using System.Globalization;
+    using System.Reflection;
+
+    using Newtonsoft.Json;
+
+    using QuantityTypes;
+
+    public class QuantityJsonConverter : JsonConverter
+    {
+        private readonly IUnitProvider unitProvider;
+
+        public QuantityJsonConverter(IUnitProvider unitProvider)
+        {
+            this.unitProvider = unitProvider;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var q = value as IQuantity;
+            if (q == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+
+            serializer.Serialize(writer, q.ToString(null, CultureInfo.InvariantCulture));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var value = serializer.Deserialize<string>(reader);
+            if (value == null)
+            {
+                return null;
+            }
+
+            IQuantity q;
+            if (this.unitProvider.TryParse(objectType, value, CultureInfo.InvariantCulture, out q))
+            {
+                return q;
+            }
+
+            throw new FormatException("Cannot parse " + value + " to " + objectType);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            var realType = Nullable.GetUnderlyingType(objectType) ?? objectType;
+            return typeof(IQuantity).GetTypeInfo().IsAssignableFrom(realType);
+        }
+    }
+}
